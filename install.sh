64cc4244729aedc46f1a2736e2369a67dc75bc39
liudonghua123 <liudonghua123@gmail.com>
2020-02-17 12:15:36 +0800

    update script

--- install.sh
+++ install.sh
@@ -1,24 +1,39 @@
 #!/usr/bin/env bash
 
-CONFIG_FILE=config.properties
-
 # read property utility
+CONFIG_FILE=config.properties
 function get_property
 {
     grep "^$1=" $CONFIG_FILE | cut -d'=' -f2-
-}
-
-function cleanup
-{
-    rm -rf /opt/credentials
-    rm -rf /opt/apache-tomcat-8.5.51
-    rm -rf /opt/shibboleth-idp
 }
 
 function get_os
 {
     cat /etc/os-release | grep "^ID=" |  cut -d= -f2-
 }
+
+# read properties as bash variable
+credentials_key_path=$(get_property credentials.key.path)
+credentials_certs_path=$(get_property credentials.certs.path)
+credentials_keystore_path=$(get_property credentials.keystore.path)
+credentials_keystore_password=$(get_property credentials.keystore.password)
+idp_src_dir=$(get_property idp.src.dir)
+idp_target_dir=$(get_property idp.target.dir)
+idp_host_name=$(get_property idp.host.name)
+idp_scope=$(get_property idp.scope)
+ldap_ip=$(get_property ldap.ip)
+idp_authn_LDAP_ldapURL=$(get_property idp.authn.LDAP.ldapURL)
+idp_authn_LDAP_baseDN=$(get_property idp.authn.LDAP.baseDN)
+idp_authn_LDAP_userFilter=$(get_property idp.authn.LDAP.userFilter)
+idp_authn_LDAP_bindDN=$(get_property idp.authn.LDAP.bindDN)
+idp_authn_LDAP_bindDNCredential=$(get_property idp.authn.LDAP.bindDNCredential)
+
+# define the filename const
+IDP_FILENAME=shibboleth-identity-provider-3.4.6
+TOMCAT_FILENAME=apache-tomcat-8.5.51
+
+# get the os, like ubuntu, centos
+OS=$(get_os)
 
 function set_java_environment
 {
@@ -36,7 +51,6 @@ function check_java
     if [ $? -ne 0 ]; then
         echo "You should install java and set JAVA_HOME/JRE_HOME correctly before this operation"
         echo "Now install java..."
-        OS=$(get_os)
         case "$OS" in
         ubuntu)
             apt-get update
@@ -61,7 +75,6 @@ function check_java
 
 function install_ldap_util
 {
-    OS=$(get_os)
     case "$OS" in
     ubuntu)
         apt-get update
@@ -82,64 +95,64 @@ function install_credentials
 {
     echo "checking and creating credentials keystore!"
     # check whether credentials exist
-    if [ ! -f "$(get_property credentials.keystore.path)" ]; then
+    if [ ! -f "$credentials_keystore_path" ]; then
         mkdir -p /opt/credentials 2>/dev/null
-        echo "keystore $(get_property credentials.keystore.path) not exist, try to generate from pem format of $(get_property credentials.key.path) and $(get_property credentials.certs.path)"
-        if [ ! -f "$(get_property credentials.key.path)" ]; then
-            echo "key $(get_property credentials.key.path) not exits, will generate a self signed key and cert pair"
-            openssl req -x509 -sha256 -nodes -days 3650 -subj "/CN=*.ynu.edu.cn"  -newkey rsa:2048 -keyout $(get_property credentials.key.path) -out $(get_property credentials.certs.path)
+        echo "keystore $credentials_keystore_path not exist, try to generate from pem format of $credentials_key_path and $credentials_certs_path"
+        if [ ! -f "$credentials_key_path" ]; then
+            echo "key $credentials_key_path not exits, will generate a self signed key and cert pair"
+            openssl req -x509 -sha256 -nodes -days 3650 -subj "/CN=*.ynu.edu.cn"  -newkey rsa:2048 -keyout $credentials_key_path -out $credentials_certs_path
         fi
         # now the key and cert are prepared
-        openssl pkcs12 -export -out $(get_property credentials.keystore.path) -inkey $(get_property credentials.key.path) -in $(get_property credentials.certs.path) -passout pass:changeit
+        openssl pkcs12 -export -out $credentials_keystore_path -inkey $credentials_key_path -in $credentials_certs_path -passout pass:changeit
     fi
     echo "credentials keystore prepared!"
 }
 
 function install_tomcat
 {
-    echo "extract apache-tomcat-8.5.51.tar.gz to /opt/"
-    tar -xzf apache-tomcat-8.5.51.tar.gz -C /opt/
+    echo "extract $TOMCAT_FILENAME.tar.gz to /opt/"
+    tar -xzf $TOMCAT_FILENAME.tar.gz -C /opt/
 }
 
 function install_idp
 {
-    echo "extract shibboleth-identity-provider-3.4.6 to $(get_property idp.target.dir)"
-    tar -xzf shibboleth-identity-provider-3.4.6.tar.gz -C $(get_property idp.target.dir)
+    echo "extract $IDP_FILENAME.tar.gz to /opt"
+    tar -xzf $IDP_FILENAME.tar.gz -C /opt
     # run install of idp
-    echo "install the shibboleth-identity-provider-3.4.6 to $(get_property idp.target.dir)"
-    echo execute /opt/shibboleth-identity-provider-3.4.6/bin/install.sh \
-        -Didp.src.dir=$(get_property idp.src.dir) \
-        -Didp.target.dir=$(get_property idp.target.dir) \
-        -Didp.host.name=$(get_property idp.host.name) \
-        -Didp.entityID="https://$(get_property idp.scope)/idp/shibboleth" \
-        -Didp.scope=$(get_property idp.scope) \
-        -Didp.keystore.password=$(get_property credentials.keystore.password) \
-        -Didp.sealer.password=$(get_property credentials.keystore.password)
-    /opt/shibboleth-identity-provider-3.4.6/bin/install.sh \
-        -Didp.src.dir=$(get_property idp.src.dir) \
-        -Didp.target.dir=$(get_property idp.target.dir) \
-        -Didp.host.name=$(get_property idp.host.name) \
-        -Didp.entityID="https://$(get_property idp.scope)/idp/shibboleth" \
-        -Didp.scope=$(get_property idp.scope) \
-        -Didp.keystore.password=$(get_property credentials.keystore.password) \
-        -Didp.sealer.password=$(get_property credentials.keystore.password)
+    echo "install the $IDP_FILENAME to $idp_target_dir"
+    echo execute /opt/$IDP_FILENAME/bin/install.sh \
+        -Didp.src.dir=$idp_src_dir \
+        -Didp.target.dir=$idp_target_dir \
+        -Didp.host.name=$idp_host_name \
+        -Didp.entityID="https://$idp_scope/idp/shibboleth" \
+        -Didp.scope=$idp_scope \
+        -Didp.keystore.password=$credentials_keystore_password \
+        -Didp.sealer.password=$credentials_keystore_password
+    /opt/$IDP_FILENAME/bin/install.sh \
+        -Didp.src.dir=$idp_src_dir \
+        -Didp.target.dir=$idp_target_dir \
+        -Didp.host.name=$idp_host_name \
+        -Didp.entityID="https://$idp_scope/idp/shibboleth" \
+        -Didp.scope=$idp_scope \
+        -Didp.keystore.password=$credentials_keystore_password \
+        -Didp.sealer.password=$credentials_keystore_password
 }
 
 function update_tomcat_credential_settings
 {
     echo "update credentials.keystore.path/credentials.keystore.password settings in tomcat configuration files"
-    credentials_keystore_path=$(get_property credentials.keystore.path)
-    credentials_keystore_password=$(get_property credentials.keystore.password)
+    credentials_keystore_path=$credentials_keystore_path
+    credentials_keystore_password=$credentials_keystore_password
     # https://linuxize.com/post/how-to-use-sed-to-find-and-replace-string-in-files/
-    grep -rl "#credentials.keystore.path" /opt/apache-tomcat-8.5.51 | xargs sed -i "s|#credentials.keystore.path|${credentials_keystore_path}|g" 
-    grep -rl "#credentials.keystore.password" /opt/apache-tomcat-8.5.51 | xargs sed -i "s/#credentials.keystore.password/${credentials_keystore_password}/g" 
+    grep -rl "#credentials.keystore.path" /opt/$TOMCAT_FILENAME | xargs sed -i "s|#credentials.keystore.path|${credentials_keystore_path}|g" 
+    grep -rl "#credentials.keystore.password" /opt/$TOMCAT_FILENAME | xargs sed -i "s/#credentials.keystore.password/${credentials_keystore_password}/g" 
 }
 
 function checking_ldap_connectivity
 {
     echo "trying to connect ldap to test its connectivity"
     install_ldap_util
-    ldapwhoami -h $(get_property ldap.ip) -D $(get_property idp.authn.LDAP.bindDN) -w $(get_property idp.authn.LDAP.bindDNCredential)
+    ldapwhoami -h $ldap_ip -D $idp_authn_LDAP_bindDN -w $idp_authn_LDAP_bindDNCredential
     $? && echo "ldap connection seems not correct" ||echo "ldap connection ok"
 }
 
@@ -147,27 +160,27 @@ function update_idp_configurations
 {
     echo update_ldap_settings_of_idp
     # backup
-    cp /opt/shibboleth-idp/conf/ldap.properties /opt/shibboleth-idp/conf/ldap.properties.default
-    cp /opt/shibboleth-idp/conf/attribute-resolver.xml /opt/shibboleth-idp/conf/attribute-resolver.xml.default
-    cp /opt/shibboleth-idp/conf/audit.xml /opt/shibboleth-idp/conf/audit.xml.default
-    cp /opt/shibboleth-idp/conf/metadata-providers.xml /opt/shibboleth-idp/conf/metadata-providers.xml.default
+    cp $idp_target_dir/conf/ldap.properties $idp_target_dir/conf/ldap.properties.default
+    cp $idp_target_dir/conf/attribute-resolver.xml $idp_target_dir/conf/attribute-resolver.xml.default
+    cp $idp_target_dir/conf/audit.xml $idp_target_dir/conf/audit.xml.default
+    cp $idp_target_dir/conf/metadata-providers.xml $idp_target_dir/conf/metadata-providers.xml.default
     # use configurated files
-    cp conf/ldap.properties /opt/shibboleth-idp/conf/ldap.properties
-    cp conf/attribute-filter.xml /opt/shibboleth-idp/conf/attribute-filter.xml
-    cp conf/attribute-resolver.xml /opt/shibboleth-idp/conf/attribute-resolver.xml
-    cp conf/audit.xml /opt/shibboleth-idp/conf/audit.xml
-    cp conf/metadata-providers.xml /opt/shibboleth-idp/conf/metadata-providers.xml
+    cp conf/ldap.properties $idp_target_dir/conf/ldap.properties
+    cp conf/attribute-filter.xml $idp_target_dir/conf/attribute-filter.xml
+    cp conf/attribute-resolver.xml $idp_target_dir/conf/attribute-resolver.xml
+    cp conf/audit.xml $idp_target_dir/conf/audit.xml
+    cp conf/metadata-providers.xml $idp_target_dir/conf/metadata-providers.xml
     # updating...
-    grep -rl "#idp.authn.LDAP.ldapURL" /opt/shibboleth-idp/conf | xargs sed -i "s|#idp.authn.LDAP.ldapURL|$(get_property idp.authn.LDAP.ldapURL)|g"
-    grep -rl "#idp.authn.LDAP.baseDN" /opt/shibboleth-idp/conf | xargs sed -i "s|#idp.authn.LDAP.baseDN|$(get_property idp.authn.LDAP.baseDN)|g"
-    grep -rl "#idp.authn.LDAP.userFilter" /opt/shibboleth-idp/conf | xargs sed -i "s|#idp.authn.LDAP.userFilter|$(get_property idp.authn.LDAP.userFilter)|g"
-    grep -rl "#idp.authn.LDAP.bindDNCredential" /opt/shibboleth-idp/conf | xargs sed -i "s|#idp.authn.LDAP.bindDNCredential|$(get_property idp.authn.LDAP.bindDNCredential)|g"
-    grep -rl "#idp.authn.LDAP.bindDN" /opt/shibboleth-idp/conf | xargs sed -i "s|#idp.authn.LDAP.bindDN|$(get_property idp.authn.LDAP.bindDN)|g"
+    grep -rl "#idp.authn.LDAP.ldapURL" $idp_target_dir/conf | xargs sed -i "s|#idp.authn.LDAP.ldapURL|$idp_authn_LDAP_ldapURL|g"
+    grep -rl "#idp.authn.LDAP.baseDN" $idp_target_dir/conf | xargs sed -i "s|#idp.authn.LDAP.baseDN|$idp_authn_LDAP_baseDN|g"
+    grep -rl "#idp.authn.LDAP.userFilter" $idp_target_dir/conf | xargs sed -i "s|#idp.authn.LDAP.userFilter|$idp_authn_LDAP_userFilter|g"
+    grep -rl "#idp.authn.LDAP.bindDNCredential" $idp_target_dir/conf | xargs sed -i "s|#idp.authn.LDAP.bindDNCredential|$idp_authn_LDAP_bindDNCredential|g"
+    grep -rl "#idp.authn.LDAP.bindDN" $idp_target_dir/conf | xargs sed -i "s|#idp.authn.LDAP.bindDN|$idp_authn_LDAP_bindDN|g"
 }
 
 function install_service
 {
-    export CATALINA_HOME=/opt/apache-tomcat-8.5.51
+    export CATALINA_HOME=/opt/$TOMCAT_FILENAME
     which systemctl
     if [ "$?" -eq 0 ]; then
         echo -n "# Systemd unit file for tomcat
